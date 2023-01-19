48b9d495fa6c99eff9bd749dbbc67aac10c2de07
Yuliya <i@yzubkova.ru>
2023-01-19 22:34:41 +0300

    Задача на определение четное ли число

---
+++ Task_6/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.WriteLine("Введите число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if (number%2 == 0)
+{
+    Console.WriteLine("Данное число является четным");
+}
+else
+{
+  Console.WriteLine("Данное число является нечетным");  
+}
