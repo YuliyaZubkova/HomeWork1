e578b18e3de252a985877ddbed88a20c45788c68
Yuliya <i@yzubkova.ru>
2023-01-19 21:56:02 +0300

    Задача про меньшее и большее число

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/ 
+
+Console.WriteLine("Введите первое число: ");
+string number1 = Console.ReadLine();
+int numberOne = Convert.ToInt32(number1);
+
+Console.WriteLine("Введите второе число: ");
+string number2 = Console.ReadLine();
+int numberTwo = Convert.ToInt32(number2);
+
+if(numberOne > numberTwo)
+{
+    Console.WriteLine("Большее число - "+numberOne);
+    Console.WriteLine("Меньшее число - "+numberTwo);
+}
+
+if(numberOne < numberTwo)
+{
+    Console.WriteLine("Большее число - "+numberTwo);
+    Console.Write("Меньшее число - "+numberOne);
+}
+if(numberOne == numberTwo)
+{
+    Console.WriteLine("Числа равны");
+}
